49e0544c66fafeef6f0752d87696e14eddbdba12
Comiscience <hu-beau@outlook.com>
2023-09-08 11:44:59 +0800

    working in progress

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,29 +39,65 @@ resource "azurerm_container_app_environment" "featbit" {
   # log_analytics_workspace_id = azurerm_log_analytics_workspace.featbit.id
 }
 
-resource "azurerm_container_app" "api_server" {
-  name                         = var.container_name.api
+resource "azurerm_container_app" "da_server" {
+  name                         = var.container_name.da_server
   container_app_environment_id = azurerm_container_app_environment.featbit.id
   resource_group_name          = azurerm_resource_group.featbit.name
   revision_mode                = "Single"
 
   template {
     container {
-      name   = var.container_name.api
-      image  = "docker.io/featbit/featbit-ui:latest"
+      name   = var.container_name.da_server
+      image  = "docker.io/featbit/featbit-data-analytics-server:latest"
       cpu    = 0.25
       memory = "0.5Gi"
+      min_replicas = 1
+      max_replicas = 3
+      env {
+        name = "REDIS_URL"
+        value = var.redis.connection_str
+      }
+      env {
+        name = "MONGO_URI"
+        value =var.mongodb.connection_str
+      }
+      env {
+        name = "MONGO_INITDB_DATABASE"
+        value = var.mongodb.db_name
+      }
+      env {
+        name = "MONGO_HOST"
+        value = "mongodb"
+      }
+    }
+  }
+}
+
+resource "azurerm_container_app" "api_server" {
+  name                         = var.container_name.api_server
+  container_app_environment_id = azurerm_container_app_environment.featbit.id
+  resource_group_name          = azurerm_resource_group.featbit.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = var.container_name.api_server
+      image  = "docker.io/featbit/featbit-api-server:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+      min_replicas = 1
+      max_replicas = 3
       env {
         name = "MongoDb__ConnectionString"
-        value = "mongodb://admin:password@mongodb:27017"
+        value = var.mongodb.connection_str
       }
       env {
         name = "MongoDb__Database"
-        value = "featbit"
+        value = var.mongodb.db_name
       }
       env {
         name = "Redis__ConnectionString"
-        value = "redis:6379"
+        value = var.redis.connection_str
       }
       env {
         name = "OLAP__ServiceHost"
@@ -69,6 +105,10 @@ resource "azurerm_container_app" "api_server" {
       }
     }
   }
+
+  depends_on = [
+    azurerm_container_app.da_server
+  ]
 }
 
 # resource "azurerm_container_app" "ui" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,14 +13,34 @@ variable "container_apps_environment" {
 variable "container_name" {
   type = object({
     ui   = string
-    api  = string
-    eval = string
-    da   = string
+    api_server  = string
+    eval_server = string
+    da_server   = string
   })
   default = {
     ui   = "ui"
-    api  = "api-server"
-    eval = "evaluation-server"
-    da   = "da-server"
+    api_server  = "api-server"
+    eval_server = "evaluation-server"
+    da_server   = "da-server"
+  }
+}
+
+variable "mongodb" {
+  type = object({
+    connection_str   = string
+    db_name  = string
+  })
+  default = {
+    connection_str   = "mongodb://admin:password@mongodb:27017"
+    db_name  = "featbit"
+  }
+}
+
+variable "redis" {
+  type = object({
+    connection_str   = string
+  })
+  default = {
+    connection_str   = "http://redis:6379"
   }
 }
